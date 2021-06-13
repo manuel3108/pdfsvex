b713390fbaeeb87fe928fc884e597d013d0c0686
Manuel Serret <mserret99@gmail.com>
2021-06-13 09:40:59 +0200

    update website

--- packages/website/svelte.config.js
+++ packages/website/svelte.config.js
@@ -1,9 +1,13 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+import staticAdapter from '@sveltejs/adapter-static';
+
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        adapter: staticAdapter(),
+        target: '#svelte',
+    },
 };
 
 export default config;
