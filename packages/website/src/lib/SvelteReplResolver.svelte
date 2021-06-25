1bb7ca9e5d6a6206ad1440e61e8d3f968f63fec7
Manuel Serret <mserret99@gmail.com>
2021-06-25 19:02:37 +0200

    add big repl

--- packages/website/src/lib/SvelteReplResolver.svelte
+++ packages/website/src/lib/SvelteReplResolver.svelte
@@ -4,12 +4,14 @@
     import { ATTRIBUTE_NAME_COMPONENTS } from './Constants';
     import 'codemirror/lib/codemirror.css';
     import 'codemirror/theme/monokai.css';
+    import clickOutside from 'svelte-outside-click';
 
     export let element;
     export let pages;
     export let allContent;
 
     let components;
+    let showBig = false;
 
     onMount(() => {
         components = JSON.parse(
@@ -17,6 +19,79 @@
         );
     });
 
+    function makeBigger() {
+        showBig = true;
+    }
+
+    function closeModal() {
+        showBig = false;
+    }
 </script>
 
-<AlternateSvelteRepl {components} height="600px" />
+<div class="wrapper">
+    <AlternateSvelteRepl {components} height="600px" />
+    <div class="flyover" style="">
+        <img on:click={makeBigger} src="/fullscreen.png" alt="fullscreen" />
+    </div>
+</div>
+
+{#if showBig}
+    <div class="modal is-active">
+        <div class="modal-background" on:click={closeModal} />
+        <div class="modal-content">
+            <AlternateSvelteRepl {components} height="100%" />
+        </div>
+    </div>
+{/if}
+
+<style>
+    .wrapper {
+        position: relative;
+    }
+
+    .flyover {
+        position: absolute;
+        right: 6px;
+        bottom: 0;
+    }
+    img {
+        cursor: pointer;
+    }
+
+    img:hover {
+        background-color: grey;
+    }
+
+    .modal {
+        align-items: center;
+        display: none;
+        flex-direction: column;
+        justify-content: center;
+        overflow: hidden;
+        position: fixed;
+        z-index: 40;
+    }
+
+    .modal.is-active {
+        display: flex;
+    }
+
+    .modal,
+    .modal-background {
+        bottom: 0;
+        left: 0;
+        position: fixed;
+        right: 0;
+        top: 0;
+    }
+
+    .modal-background {
+        background-color: rgba(58, 58, 58, 0.86);
+    }
+
+    .modal-content {
+        z-index: 50;
+        width: 80%;
+        height: 80%;
+    }
+</style>
