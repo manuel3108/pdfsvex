ebb5973944d972feb692a4d57dd2fedb587005ea
Manuel Serret <mserret99@gmail.com>
2021-06-25 18:46:48 +0200

    improve topbar

--- packages/alternate-svelte-repl/src/TopBar.svelte
+++ packages/alternate-svelte-repl/src/TopBar.svelte
@@ -16,25 +16,80 @@
             newIndex: index,
         });
     }
-
 </script>
 
-{#each components as component, i}
-    <button
-        class:color={i === currentComponentIndex}
-        on:click={() => onClick(i)}
-    >
-        {component.name}.{component.type}
-    </button>
-{/each}
+<div class="container">
+    <div class="column">
+        {#each components as component, i}
+            <div
+                class="button"
+                class:active={i === currentComponentIndex}
+                on:click={() => onClick(i)}
+            >
+                <span>{component.name}.{component.type}</span>
+            </div>
+        {/each}
+    </div>
 
-<button on:click={() => ($view = 'full')}>full</button>
-<button on:click={() => ($view = 'preview')}>preview</button>
-<button on:click={() => ($view = 'editor')}>editor</button>
+    <div class="column">
+        <div
+            class="button"
+            class:active={$view === 'full'}
+            on:click={() => ($view = 'full')}
+        >
+            <span>full</span>
+        </div>
+        <div
+            class="button"
+            class:active={$view === 'preview'}
+            on:click={() => ($view = 'preview')}
+        >
+            <span>preview</span>
+        </div>
+        <div
+            class="button"
+            class:active={$view === 'editor'}
+            on:click={() => ($view = 'editor')}
+        >
+            <span>editor</span>
+        </div>
+    </div>
+</div>
 
 <style>
-    .color {
-        background-color: red;
+    .column {
+        display: inline-flex;
     }
 
+    .button {
+        cursor: pointer;
+        background-color: #aaa;
+        height: 100%;
+    }
+
+    .button span {
+        vertical-align: middle;
+    }
+
+    .container {
+        background-color: #bbb;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        border-collapse: collapse;
+    }
+
+    .column .button {
+        padding-left: 5px;
+        padding-right: 5px;
+        border-left: 1px solid #333;
+    }
+
+    .column .button:last-child {
+        border-right: 1px solid #333;
+    }
+
+    .button.active {
+        background-color: #888;
+    }
 </style>
